a2d437fe99abd26b526cca4901d17851f6e5d851
Elliot Winkler <elliot.winkler@gmail.com>
2009-07-28 11:32:36 -0500

    Add :only and :except to controller class methods to limit setting the title to specific actions

--- lib/mcmire/title_helpers/controller.rb
+++ lib/mcmire/title_helpers/controller.rb
@@ -10,29 +10,36 @@ module Mcmire
         # the window title before each action in your controller. This will work
         # for subcontrollers too -- so if you call window_title in a supercontroller
         # and also in a subcontroller, there will be two strings when you go to output
-        # the window title.
+        # the window title. You can pass :only and :except to limit this to certain actions.
         def window_title(*args, &block)
-          before_filter {|c| c.window_title(*args, &block) }
+          options = args.extract_options!
+          before_filter(options) {|c| c.window_title(*args, &block) }
         end
       
         # Call this in the body of your controller with a string to set the page title
         # globally for each action in your controller. Unlike window_title, this will not
         # work for subcontrollers -- so if you call page_title in a supercontroller and
         # also in a subcontroller, the subcontroller's title will override the supercontroller's.
+        # You can pass :only and :except to limit this to certain actions.
         def page_title(*args, &block)
-          before_filter {|c| c.page_title(*args, &block) }
+          options = args.extract_options!
+          before_filter(options) {|c| c.page_title(*args, &block) }
         end
       
         # Call this in the body of your controller to tell page_title to not show anything.
+        # You can pass :only and :except to limit this to certain actions.
         def hide_page_title(*args, &block)
-          before_filter {|c| c.hide_page_title(*args, &block) }
+          options = args.extract_options!
+          before_filter(options) {|c| c.hide_page_title(*args, &block) }
         end
       
         # Call this in the body of your controller with a string to add it to the
         # window title AND set the page title at the same time.
         # See +window_title+ and +page_title+ for more.
+        # You can pass :only and :except to limit this to certain actions.
         def title(*args, &block)
-          before_filter {|c| c.title(*args, &block) }
+          options = args.extract_options!
+          before_filter(options) {|c| c.title(*args, &block) }
         end
       end
       
