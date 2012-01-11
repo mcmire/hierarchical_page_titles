464c6bf136aa9c688b4eb1c02bfc8b92bdd96c88
Elliot Winkler <elliot.winkler@gmail.com>
2012-01-11 11:59:58 -0700

    Correct railtie namespace

--- lib/hierarchical_page_titles/railtie.rb
+++ lib/hierarchical_page_titles/railtie.rb
@@ -6,7 +6,7 @@ require 'hierarchical_page_titles/view_helpers'
 module HierarchicalPageTitles
   class Railtie < Rails::Railtie
     # copied from formtastic
-    initializer "title-helpers.initialize", :after => :after_initialize do
+    initializer "hierarchical_page_titles.initialize", :after => :after_initialize do
       ActionController::Base.class_eval { include HierarchicalPageTitles::ControllerHelpers }
       ActionView::Base.class_eval { include HierarchicalPageTitles::ViewHelpers }
     end
