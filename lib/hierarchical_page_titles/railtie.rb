23cddad4e566e5db802e0c3a529963b590c32cc1
Elliot Winkler <elliot.winkler@gmail.com>
2012-04-25 11:06:18 -0600

    Add an actual initializer to the railtie

--- lib/hierarchical_page_titles/railtie.rb
+++ lib/hierarchical_page_titles/railtie.rb
@@ -8,12 +8,14 @@ module HierarchicalPageTitles
   require libpath('hierarchical_page_titles/view_helpers')
 
   class Railtie < Rails::Railtie
-    ActiveSupport.on_load(:action_controller) do
-      include HierarchicalPageTitles::ControllerHelpers
-    end
+    initializer 'hierarchical_page_titles.hook' do
+      ActiveSupport.on_load(:action_controller) do
+        include HierarchicalPageTitles::ControllerHelpers
+      end
 
-    ActiveSupport.on_load(:action_view) do
-      include HierarchicalPageTitles::ViewHelpers
+      ActiveSupport.on_load(:action_view) do
+        include HierarchicalPageTitles::ViewHelpers
+      end
     end
   end
 end
