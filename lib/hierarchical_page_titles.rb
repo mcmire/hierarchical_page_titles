cc6cc2187ab1ca3fa1bcf1161e84a52084568162
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-29 20:25:17 +0800

    Delete lib directory

--- lib/hierarchical_page_titles.rb
+++
@@ -1,6 +0,0 @@
-
-require_relative 'hierarchical_page_titles/paths'
-
-module HierarchicalPageTitles
-  require libpath('hierarchical_page_titles/railtie')
-end
--- lib/hierarchical_page_titles/version.rb
+++
@@ -1,5 +0,0 @@
-
-module HierarchicalPageTitles
-  VERSION = "0.2.0"
-  def self.version; VERSION; end
-end
